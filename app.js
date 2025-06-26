b0885496462012f12279a3a1424ca3acd85c91e4
Oleksandr kodehode <oleksandr.kodehode@gmail.com>
2025-06-26 09:06:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+/******************************************************************************
+OPPGAVE 1
+
+Din første oppgave er å koble denne JavaScript-filen til index.html-filen
+ved å bruke en av metodene vi viste i første forelesning.
+
+<-- Finn index.html-filen i filutforskeren og koble den til denne filen,
+javascript.js
+******************************************************************************/
+
+// Løs denne oppgaven i index.html
+
+/******************************************************************************
+OPPGAVE 2
+
+I forrige undervisning lærte vi hvordan man lager variabler som kan holde ulike
+typer verdier. Lag noen variabler med følgende datatyper:
+- String (tekst)
+- Number (tall)
+- Boolean (sann/usann)
+- Array (liste)
+
+Du kan velge hva innholdet i variablene skal være. Prøv å bruke både let og 
+const når du definerer variablene.
+******************************************************************************/
+
+let exampleText = "hello";
+let exampleNumber = 1;
+let exampleBoolean = true;
+let exampleArray = [1, "hello, world", true];
+
+/******************************************************************************
+OPPGAVE 3
+
+Prøv ut noen av operatorene vi så på i forrige forelesning:
+- Matematiske operatorer: +, -, /, *
+- Forkortede operatorer: ++, --, +=, -=
+
+Skriv noen eksempler der du tester disse operatorene.
+******************************************************************************/
+
+let numOne = 5;
+let numTwo = 10;
+
+console.log(numOne * numTwo + numTwo / numOne);
+console.log(numTwo++, numOne--);
+/******************************************************************************
+OPPGAVE 4
+
+Skriv en IF/ELSE-betingelse som sjekker følgende:
+1. At userName ikke er tom ("").
+2. At userAge er 18 eller eldre.
+3. At userIsBlocked er false.
+
+(TIPS: Bruk && (logisk OG) for å sjekke alle tre betingelsene i én IF-setning.)
+
+- Hvis alle disse betingelsene er oppfylt, skal du sette variabelen
+userIsLoggedIn til true og goToPage til "/home". Deretter skriver du ut en 
+velkomstmelding med console.log.
+
+- Hvis noen av betingelsene IKKE er oppfylt, skal du skrive ut en feilmelding
+med console.log.
+
+Prøv å endre verdiene på variablene for å sikre at IF/ELSE-setningen din 
+håndterer alle tilfeller korrekt.
+******************************************************************************/
+
+let userName = "";
+let userAge = 18;
+let userIsLoggedIn = false;
+let userIsBlocked = false;
+let goToPage = "";
+
+const valid = userName !== "" && userAge >= 18 && !userIsBlocked;
+
+if (valid) {
+  userIsLoggedIn = true;
+  goToPage = "/home";
+  console.log("Welcome!");
+} else {
+  console.log("Error");
+}
+
+/******************************************************************************
+OPPGAVE 5
+
+Lag en variabel kalt userTitle og sett innholdet til å være:
+- "Mr." hvis userMale er true, eller
+- "Mrs." hvis userMale er false.
+
+Bruk en ternary conditional for dette:
+
+const variabel = betingelse ? "hvis sann" : "hvis usann";
+
+Prøv å endre userMale til både true og false og bruk console.log for å sjekke
+at betingelsen din fungerer som den skal.
+******************************************************************************/
+
+const userMale = true;
+
+if (userMale) {
+  console.log("Mr.");
+} else {
+  console.log("Mrs.");
+}
